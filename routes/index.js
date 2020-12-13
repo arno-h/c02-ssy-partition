d392211af65a5ebed6ab75f89d3e2a52593f2068
Arno Hollosi <arno.hollosi@campus02.at>
2020-12-13 11:14:41 +0100

    Datenbanken harmonisiert; index.js gefixt

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cluster = require('cluster');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Worker.ID: ' + cluster.worker.id);
+router.get('/', function (req, res) {
+    if (cluster.isMaster) {
+        res.send('Masternode up & running')
+    } else {
+        res.send('Worker.ID: ' + cluster.worker.id);
+    }
 });
 
 module.exports = router;
--- src/mono-database.js
+++ src/mono-database.js
@@ -1,10 +1,12 @@
 const Loki = require("lokijs");
-
 const db = new Loki('mono.json');
-
 const mono = db.addCollection('mono');
-mono.insert({key: 'key1', value: 'value one'});
-mono.insert({key: 'key2', value: 'value two'});
-mono.insert({key: 'key3', value: 'value three'});
+
+// Erzeugt 26 Einträge der Form {"ainhorn": "einhorn-a"} analog zu den Partitionen von multi/peer
+let chr = 'abcdefghijklmnopqrstuvwxyz';
+for (let i=0; i < 26; i++) {
+    mono.insert({key: chr[i] + "inhorn", value: 'einhorn-' + chr[i]});
+    mono.insert({key: chr[i] + "lefant", value: 'elefant-' + chr[i]});
+}
 
 module.exports = db;
